8e13596aed28710388597c1903b872791f718c1d
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2025-08-11 17:08:26 -0500

    se crean los botones de copia y restauracion de seguridad

--- reportes_bascula.py
+++ reportes_bascula.py
@@ -3,9 +3,14 @@
 import tkinter as tk  # Para la interfaz gráfica
 from tkinter import ttk, messagebox, filedialog  # Widgets y mensajes de Tkinter
 from datetime import datetime  # Para manejar fechas
+from datetime import timedelta #para tomar reportes del ultimo dia
+from tkcalendar import Calendar #para usar calendario
 import mysql.connector  # Conexión a MySQL
 import pandas as pd  # Manipulación de datos y exportación a Excel
-import os  # Operaciones con archivos
+import os  # Operaciones con archivos y deteccion de ejecutables
+import tkinter.font as tkFont #para autoajustar las columnas
+import subprocess #importo subprocesos para realizar copias de seguridad de Mysql
+
 
 # Importa pyodbc si está disponible, para exportar a Access
 try:
@@ -21,10 +26,54 @@ except ImportError:
 
 # === Clase principal para la interfaz de reportes ===
 class ReportesBasculaApp:
+    
+    #funcion para seleccionar fechas en calendario
+    def seleccionar_fecha(self, entry_widget):
+        top = tk.Toplevel(self.root)
+        cal = Calendar(top, date_pattern="yyyy-mm-dd")
+        cal.pack(pady=10)
+
+        def confirmar():
+            entry_widget.delete(0, tk.END)
+            entry_widget.insert(0, cal.get_date())
+            top.destroy()
+
+        ttk.Button(top, text="OK", command=confirmar).pack()
+    
+    # Constructor de clase ReportesBasculaApp.
+    """
+    - Inicializa la ventana principal y todos los elementos de la interfaz gráfica:
+    - Configuración inicial de la ventana (título, tamaño).
+    - Creación de filtros de búsqueda (tipo de reporte, fechas, cliente, etc.).
+    - Configuración de botones de consulta y exportación (Excel, Access, PDF).
+    - Creación de la tabla Treeview con scrollbars para mostrar resultados.
+    - Definición de variables internas para manejo de datos.
+    """
     def __init__(self, root):
+        
+        self.db_password = "bascula2025"  # guardo la contraseña de MySQL dentro de la clase para ser usada
+        
         self.root = root
         self.root.title("Consulta y Reportes - Báscula")
         self.root.geometry("1100x650")  # Tamaño inicial de ventana
+        #desde aqui hubico la ventana en la mitad de la pantalla
+        # Tamaño inicial
+        ancho_ventana = 1100
+        alto_ventana = 650
+
+        # Obtener dimensiones de la pantalla
+        ancho_pantalla = self.root.winfo_screenwidth()
+        alto_pantalla = self.root.winfo_screenheight()
+
+        # Calcular posición x, y para centrar
+        pos_x = int((ancho_pantalla / 2) - (ancho_ventana / 2))
+        pos_y = int((alto_pantalla / 2) - (alto_ventana / 2))
+
+        # Establecer tamaño y posición centrada
+        self.root.geometry(f"{ancho_ventana}x{alto_ventana}+{pos_x}+{pos_y}")
+        
+        #hasta aqui es para centrar el programa en la pantalla al iniciar
+        
 
         self.tipo_reporte = tk.StringVar(value="pesajes")  # Valor predeterminado
 
@@ -39,14 +88,40 @@ class ReportesBasculaApp:
 
         # Fechas de búsqueda
         # Filtro: Fecha inicial
-        ttk.Label(filtro_frame, text="Fecha inicial (YYYY-MM-DD):").grid(row=0, column=2, padx=5)
-        self.fecha_inicio = ttk.Entry(filtro_frame)
-        self.fecha_inicio.grid(row=0, column=3, padx=5)
+        ttk.Label(filtro_frame, text="Fecha inicial (YYYY-MM-DD):").grid(row=0, column=2, padx=5) #label trasparente indicar formato fecha
+        #self.fecha_inicio = ttk.Entry(filtro_frame)
+        #self.fecha_inicio.grid(row=0, column=3, padx=5)
 
+        frame_fecha_inicio = ttk.Frame(filtro_frame)  # Contenedor para campo + botón
+        frame_fecha_inicio.grid(row=0, column=3, padx=5, pady=5)
+        self.fecha_inicio = ttk.Entry(frame_fecha_inicio, width=12)
+        self.fecha_inicio.pack(side="left", fill="x", expand=True)
+
+        # Botón calendario inicio
+        #ttk.Button(filtro_frame, text="📅", width=3, command=lambda: self.seleccionar_fecha(self.fecha_inicio)).grid(row=0, column=4, padx=2)
+        ttk.Button(frame_fecha_inicio, text="📅", width=2, command=lambda: self.seleccionar_fecha(self.fecha_inicio)).pack(side="right")
+        
         # Filtro: Fecha final
-        ttk.Label(filtro_frame, text="Fecha final (YYYY-MM-DD):").grid(row=0, column=4, padx=5)
-        self.fecha_fin = ttk.Entry(filtro_frame)
-        self.fecha_fin.grid(row=0, column=5, padx=5)
+        ttk.Label(filtro_frame, text="Fecha final (YYYY-MM-DD):").grid(row=0, column=4, padx=5)#label trasparente indicar formato fecha
+        #self.fecha_fin = ttk.Entry(filtro_frame)
+        #self.fecha_fin.grid(row=0, column=5, padx=5)
+        frame_fecha_fin = ttk.Frame(filtro_frame)  # Contenedor para campo + botón
+        frame_fecha_fin.grid(row=0, column=5, padx=5, pady=5)
+        self.fecha_fin = ttk.Entry(frame_fecha_fin, width=12)
+        self.fecha_fin.pack(side="left", fill="x", expand=True)
+        
+        # Botón calendario final
+        #ttk.Button(filtro_frame, text="📅", width=3, command=lambda: self.seleccionar_fecha(self.fecha_fin)).grid(row=0, column=7, padx=2)
+        ttk.Button(frame_fecha_fin, text="📅", width=2, command=lambda: self.seleccionar_fecha(self.fecha_fin)).pack(side="right")
+        
+        #ingreso fecha actual por defecto al abrir programa
+        hoy = datetime.now().strftime("%Y-%m-%d")
+        self.fecha_inicio.insert(0, hoy)
+        self.fecha_fin.insert(0, hoy)
+        
+        # Pasar foco con Enter
+        self.fecha_inicio.bind("<Return>", lambda e: self.fecha_fin.focus_set())
+        self.fecha_fin.bind("<Return>", lambda e: self.consultar())
 
         # Filtro por placa o nombre (solo para pesaje)
         ttk.Label(filtro_frame, text="Cliente / Placa / Cedula o Nit:").grid(row=1, column=0, padx=5, pady=5)
@@ -60,22 +135,84 @@ class ReportesBasculaApp:
         self.filtro_tipo_cliente.set("Todos")  # Valor por defecto
 
         # Botón de consulta
-        ttk.Button(filtro_frame, text="Consultar", command=self.consultar).grid(row=1, column=6, padx=10)
+        ttk.Button(filtro_frame, text="Consultar", command=self.consultar).grid(row=1, column=5, padx=5, pady=5, sticky="ew")
 
-        # === Tabla para mostrar resultados ===
-        self.tree = ttk.Treeview(root)
-        self.tree.pack(fill="both", expand=True, padx=10, pady=10)
+        # === Tabla para mostrar resultados con scroll (Treeview + Scrollbars) ===
+        tree_frame = ttk.Frame(root)
+        tree_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+        # Scrollbars
+        vsb = ttk.Scrollbar(tree_frame, orient="vertical")
+        hsb = ttk.Scrollbar(tree_frame, orient="horizontal")
+
+        # Treeview con scrollbars
+        self.tree = ttk.Treeview(
+            tree_frame,
+            show="headings",
+            yscrollcommand=vsb.set,
+            xscrollcommand=hsb.set
+        )
+        self.tree.grid(row=0, column=0, sticky="nsew")
+
+        # Configuración de scrollbars
+        vsb.config(command=self.tree.yview)
+        hsb.config(command=self.tree.xview)
+        vsb.grid(row=0, column=1, sticky="ns")
+        hsb.grid(row=1, column=0, sticky="ew")
+
+        # Ajustar expansión del Treeview dentro del frame
+        tree_frame.grid_rowconfigure(0, weight=1)
+        tree_frame.grid_columnconfigure(0, weight=1)
 
         # === Botones de exportación de resultados===
+        #btn_frame = ttk.Frame(root)
+        #btn_frame.pack(pady=5)
+        
+        
+        #ttk.Button(btn_frame, text="Exportar a Excel", command=self.exportar_excel).pack(side="left", padx=5)
+        #ttk.Button(btn_frame, text="Exportar a Access", command=self.exportar_access).pack(side="left", padx=5)
+        #ttk.Button(btn_frame, text="Exportar a PDF", command=self.exportar_pdf).pack(side="left", padx=5)
+        
+        
+        # === Botones de exportación y respaldo ===
         btn_frame = ttk.Frame(root)
-        btn_frame.pack(pady=5)
+        btn_frame.pack(fill="x", pady=5)  # aquí solo colocamos el frame con pack
 
-        ttk.Button(btn_frame, text="Exportar a Excel", command=self.exportar_excel).pack(side="left", padx=5)
-        ttk.Button(btn_frame, text="Exportar a Access", command=self.exportar_access).pack(side="left", padx=5)
-        ttk.Button(btn_frame, text="Exportar a PDF", command=self.exportar_pdf).pack(side="left", padx=5)
+        # Exportar
+        ttk.Button(btn_frame, text="Exportar a Excel", command=self.exportar_excel).grid(row=0, column=0, padx=5, pady=5)
+        ttk.Button(btn_frame, text="Exportar a Access", command=self.exportar_access).grid(row=0, column=1, padx=5, pady=5)
+        ttk.Button(btn_frame, text="Exportar a PDF", command=self.exportar_pdf).grid(row=0, column=2, padx=5, pady=5)
+
+        # Espacio vacío para empujar los de respaldo
+        btn_frame.grid_columnconfigure(3, weight=1)
+        
+        # Botones de copia de seguridad y restauracion
+        
+
+        #ttk.Button(btn_frame, text="Copia de seguridad", command=self.backup_base_datos).pack(side="right", padx=5)
+        #ttk.Button(btn_frame, text="Restaurar Copia Seguridad", command=self.restaurar_base_datos).pack(side="right", padx=5)
+        
+        # Botón copia de seguridad (misma columna que Consultar, fila siguiente)
+        ttk.Button(btn_frame, text="Copia de seguridad", command=self.backup_base_datos).grid(row=0, column=3, padx=5, pady=5)
+
+        # Botón restaurar copia (misma columna que Consultar, fila siguiente)
+        ttk.Button(btn_frame, text="Restaurar Copia Seguridad", command=self.restaurar_base_datos).grid(row=0, column=5, padx=5, pady=5, sticky="ew")
+
+
+        #ttk.Button(btn_frame, text="Copia de seguridad", command=self.backup_base_datos).grid(row=0, column=5, padx=5)
+        #ttk.Button(btn_frame, text="Restaurar Copia Seguridad", command=self.restaurar_base_datos).grid(row=0, column=6, padx=5)
+
 
         self.datos_actuales = []  # Aquí se guardan los datos cargados
 
+
+     
+    
+    #funcion para ajustar las columnas
+    def autoajustar_columnas(self):
+        for col in self.tree["columns"]:
+            max_len = max([len(str(self.tree.set(k, col))) for k in self.tree.get_children()] + [len(col)])
+            self.tree.column(col, width=(max_len * 7))
 
     # === CONSULTA MYSQL ===
     def consultar(self):
@@ -91,7 +228,7 @@ class ReportesBasculaApp:
         # Validación de fechas
         try:
             fecha_inicio = datetime.strptime(fi, "%Y-%m-%d")
-            fecha_fin = datetime.strptime(ff, "%Y-%m-%d")
+            fecha_fin = datetime.strptime(ff, "%Y-%m-%d") + timedelta(days=1) - timedelta(seconds=1) #adelanto 1 dia y le resto 1 segundo para que tome registros del ultimo dia indicado
         except ValueError:
             messagebox.showerror("Error", "Formato de fecha inválido. Usa YYYY-MM-DD.")
             return
@@ -217,6 +354,7 @@ class ReportesBasculaApp:
 
 
         # Mostrar resultados en la tabla
+        # Limpiar Treeview
         self.tree.delete(*self.tree.get_children())
 
         if resultados:
@@ -244,6 +382,8 @@ class ReportesBasculaApp:
             for row in datos_modificados:
                 self.tree.insert("", "end", values=list(row.values()))
 
+            # === Ajustar ancho de columnas automáticamente ===
+            self.autoajustar_columnas()
             self.datos_actuales = resultados  # aquí guardamos el original sin modificaciones para uso interno
 
         else:
@@ -358,6 +498,7 @@ class ReportesBasculaApp:
             # Anchos personalizados (en mm) para cada columna si existe para no sobrepasar 270mm(margen incluido)
             ancho_personalizado = {
                 "id_pesaje": 30,
+                "id_ingresado": 30,
                 "nombre": 50,
                 "cedula_nit": 25,
                 "fecha_hora": 35,
@@ -392,6 +533,71 @@ class ReportesBasculaApp:
                 messagebox.showinfo("Éxito", f"Exportado a PDF: {archivo}")
             except PermissionError:
                 messagebox.showerror("Error", f"No se puede sobrescribir el PDF.\nCierra el archivo primero:\n{archivo}")
+
+    
+    # Funcion del boton de copia de seguridad de la base de datos            
+    def backup_base_datos(self):
+        archivo = filedialog.asksaveasfilename(defaultextension=".sql", filetypes=[("SQL", "*.sql")])
+        if not archivo:
+            return
+        
+        mysqldump_path = r"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe"  # Ruta completa para evitar modificar path
+        
+        # Verificar si el ejecutable existe
+        if not os.path.isfile(mysqldump_path):
+            messagebox.showerror("Error", f"No se encontró el ejecutable:\n{mysqldump_path}")
+            return  
+        
+        try:
+            comando = [
+                mysqldump_path,
+                "-h", "localhost",
+                "-u", "root",
+                f"-p{self.db_password}", # contraseña
+                "bascula_silvotecnia"
+            ]
+            with open(archivo, "w", encoding="utf-8") as f:
+                subprocess.run(comando, stdout=f, check=True)
+            messagebox.showinfo("Éxito", f"Copia de seguridad creada:\n{archivo}")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo crear la copia de seguridad.\n{e}")
+
+    
+    # Funcion del boton de Restauracion copia de seguridad de la base de datos
+    def restaurar_base_datos(self):
+        archivo = filedialog.askopenfilename(defaultextension=".sql", filetypes=[("SQL", "*.sql")])
+        if not archivo:
+            return
+        
+        mysql_path = r"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe"  # Ruta completa para evitar modificar path
+        
+            
+        # Verificar si el ejecutable existe
+        if not os.path.isfile(mysql_path):
+            messagebox.showerror("Error", f"No se encontró el ejecutable:\n{mysql_path}")
+            return
+                
+        confirmacion = messagebox.askyesno(
+            "Confirmar restauración",
+            "⚠ Esto borrará todos los datos actuales y restaurará la copia.\n¿Continuar?"
+        )
+        if not confirmacion:
+            return
+        try:
+            comando = [
+                mysql_path,
+                "-h", "localhost",
+                "-u", "root",
+                f"-p{self.db_password}",
+                "bascula_silvotecnia"
+            ]
+            with open(archivo, "r", encoding="utf-8") as f:
+                subprocess.run(comando, stdin=f, check=True)
+            messagebox.showinfo("Éxito", "Base de datos restaurada correctamente.")
+        except Exception as e:
+            messagebox.showerror("Error", f"No se pudo restaurar la base de datos.\n{e}")
+            
+
 
 
 # === Punto de entrada principal, Ejecutar interfaz si se corre el script directamente ===
